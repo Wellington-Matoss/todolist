8ed75e2d9b740d7419ae63aef92c0ff5ac0f5cfa
Wellington-Matoss <wellington.matos.neves@gmail.com>
2024-01-04 19:32:35 -0300

    as

--- source/routes/checklist.js
+++ source/routes/checklist.js
@@ -2,14 +2,30 @@ const express = require("express")
 
 const router = express.Router()
 
-router.get("/",  (req, res) =>{
+router.get("/",  (req, res) => {
     console.log("MoaMetall")
     res.send()
 } )
 
-router.post("/",  (req, res) =>{
+router.post("/",  (req, res) => {
     console.log(req.body)
     res.status(200).send(req.body)
 } )
+
+router.get("/:id",  (req, res) => {
+    console.log(req.params.id)
+    res.send(`ID:${req.params.id}`)
+} )
+
+router.put("/:id",  (req, res) => {
+    console.log(req.params.id)
+    res.send(`PUT ID:${req.params.id}`)
+} )
+
+router.delete("/:id",  (req, res) => {
+    console.log(req.params.id)
+    res.send(`Delete ID:${req.params.id}`)
+} )
+
 
 module.exports = router
