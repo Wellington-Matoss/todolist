99ddb88f15887b811c786dec375e2788164aa519
Wellington-Matoss <wellington.matos.neves@gmail.com>
2024-01-04 12:27:41 -0300

    class done

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const checkListRouter = require("./source/routes/checklist")
+
+
+const app = express();
+
+app.use(express.json())
+
+
+app.use("/checklist",checkListRouter)
+
+
+app.listen(3000, () => {
+  console.log("O servidor foi iniciado...");
+});
